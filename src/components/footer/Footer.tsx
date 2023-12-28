77493060f414793fbcbb8c412b62569242d8365f
Leonardo Bezerra <leossbezerra@gmail.com>
2023-12-28 09:21:53 -0300

    logo on footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,19 @@ import "./Footer.css"
 const Footer = () => {
 	return (
 		<div className='Footer'>
+
 			<p>Biblioteca de filmes</p>
-			<p>Imagens e textos através de <a href="https://developer.themoviedb.org/docs">TMDB API</a></p> 
+
+			<div>
+				<p>Imagens e textos através de </p> 
+
+				<a href="https://developer.themoviedb.org/docs" target="_blank">
+					<img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" 
+					alt="tmdb-logo" 
+					/>
+				</a>
+			</div>
+
 		</div>
 	)
 }
