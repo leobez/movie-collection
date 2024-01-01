0a03978ffcc8b291f1ac68db3dc8c1dc91282512
Leonardo Bezerra <leossbezerra@gmail.com>
2024-01-01 13:36:52 -0300

    Update Random.tsx

--- src/pages/random/Random.tsx
+++ src/pages/random/Random.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { useGetMovies } from "../../hooks/useGetMovies"
 import { IGenre } from "../../interfaces/Genre"
 import "./Random.css"
